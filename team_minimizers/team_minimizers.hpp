72c966cc2db8c5d845f467a45c3a5f3cb147768d
Anamarija Kic <anamarija.kic@fer.unizg.hr>
2025-05-31 23:27:15 +0200

    Mapper ak 2 (#12)
    * Faster minimizers
    
    * Minimizers in class

--- team_minimizers/team_minimizers.hpp
+++ team_minimizers/team_minimizers.hpp
@@ -4,13 +4,28 @@
 #include <vector>
 #include <tuple>
 #include <string>
+#include <deque>
 
 namespace team {
 
-std::vector<std::tuple<unsigned int, unsigned int, bool>> Minimize(
-    const char* sequence, unsigned int sequence_len,
-    unsigned int kmer_len,
-    unsigned int window_len);
+class KMER {
+    private:
+        bool is_fwd = true;
+
+    public:
+        KMER(bool is_fwd_);
+
+        std::vector<std::tuple<unsigned int, unsigned int, bool>> Minimize(
+            const char* sequence, unsigned int sequence_len,
+            unsigned int kmer_len,
+            unsigned int window_len);
+
+        std::string MappKmerBitToString(unsigned int kmer, unsigned int kmer_len);
+        unsigned int MappSeqCharPointerToBit(const char* seq, unsigned int kmer_len);
+        std::string ReverseComplement(const std::string& kmer);
+        std::tuple<unsigned int, unsigned int, bool> GetTupleWithMinFirst(
+            const std::deque<std::tuple<unsigned int, unsigned int, bool>>& window);
+    };
 }
 
 #endif
